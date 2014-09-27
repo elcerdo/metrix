8e196a2be307070c1c459f6694e1cda45552bad8
Pierre Gueth <georges.wbushjr@laposte.net>
2014-09-27 20:14:56 +0200

    material and manual object

--- main.cc
+++ main.cc
@@ -44,34 +44,75 @@ int main(int argc, char* argv[])
     if (!root->showConfigDialog()) return -1;
 
     Ogre::ResourceGroupManager::getSingleton().addResourceLocation("media", "FileSystem");
+    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
     Ogre::RenderWindow* window = root->initialise(true);
     Ogre::SceneManager* scene = root->createSceneManager(Ogre::ST_GENERIC);
 
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
     {
         Ogre::Camera* mCamera = scene->createCamera("PlayerCam");
 
-        mCamera->setPosition(Ogre::Vector3(0,0,500));
+        mCamera->setPosition(Ogre::Vector3(200,100,500));
         mCamera->lookAt(Ogre::Vector3(0,0,0));
         mCamera->setNearClipDistance(5);
 
         Ogre::Viewport* vp = window->addViewport(mCamera);
-        vp->setBackgroundColour(Ogre::ColourValue(.8,0,0));
+        vp->setBackgroundColour(Ogre::ColourValue(.2,.2,.2));
     }
 
     {
-        Ogre::ManualObject* manual = scene->createManualObject("UnicornMaterial");
-        manual->begin("FistSection", Ogre::RenderOperation::OT_LINE_STRIP);
+        Ogre::ManualObject* manual = scene->createManualObject("manual_object");
+
+        manual->begin("unicorn_mat", Ogre::RenderOperation::OT_LINE_STRIP);
         manual->position(0,100,0);
+        manual->textureCoord(0,1,0);
         manual->position(0,0,0);
+        manual->textureCoord(0,0,0);
         manual->position(100,0,0);
+        manual->textureCoord(1,0,0);
         manual->end();
-        scene->getRootSceneNode()->attachObject(manual);
+
+        manual->begin("unicorn_mat", Ogre::RenderOperation::OT_TRIANGLE_STRIP);
+        manual->position(10,110,0);
+        manual->textureCoord(0,1,0);
+        manual->position(10,10,0);
+        manual->textureCoord(0,0,0);
+        manual->position(110,110,10);
+        manual->textureCoord(1,1,0);
+        manual->position(110,10,0);
+        manual->textureCoord(1,0,0);
+        manual->index(0);
+        manual->index(1);
+        manual->index(3);
+        manual->index(0);
+        manual->index(1);
+        manual->index(2);
+        manual->end();
+
+        Ogre::SceneNode* node = scene->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(-20,0,0));
+        node->attachObject(manual);
     }
 
-    scene->setAmbientLight(Ogre::ColourValue(.5,.5,1.));
+    {
+        Ogre::MeshManager::getSingleton().createPlane(
+            "ground_plane.mesh",
+            Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+            Ogre::Plane(Ogre::Vector3::UNIT_Y, Ogre::Vector3::ZERO),
+            2000, 2000,
+            20, 20,
+            true, 1, 5, 5,
+            Ogre::Vector3::NEGATIVE_UNIT_Z
+            );
+
+        Ogre::Entity* ground = scene->createEntity("ground", "ground_plane.mesh");
+        ground->setMaterialName("ground_mat");
+
+        Ogre::SceneNode* node = scene->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(0,-30,0));
+        node->attachObject(ground);
+    }
+
+    scene->setAmbientLight(Ogre::ColourValue(.9,.9,1.));
 
     OIS::ParamList pl;
     size_t windowHnd = 0;
